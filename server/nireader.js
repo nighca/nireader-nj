ace61534d87b5096b2d5f16e62cd9c1d2da51c28
nighca <nighca@live.cn>
2013-10-29 18:12:16 +0800

    update server

--- server/nireader.js
+++ server/nireader.js
@@ -1,51 +1,45 @@
-/**
- * Module dependencies.
- */
-var express = require('express');
-var http = require('http');
-var path = require('path');
-var routes = require('./router').routes;
-
-var dealSession = require('./middleware/dealSession');
-
-require('./task');
+var express = require('express'),
+    http = require('http'),
+    path = require('path'),
+    routes = require('./router').routes,
+    task = require('./task'),
+    domains = require('./config/domain.json'),
+    cookieSessionConfig = require('./config/cookieSession.json');
 
 var app = express();
 
-app.set('port', process.env.PORT || 3000);
+app.configure(function(){
+    app.set('views', path.join(__dirname, 'view'));
+    app.set('view engine', 'jade');
+    
+    app.use(express.static(path.join(__dirname, '../static')));
 
-var domains = require('./config/domain.json');
-app.set('views', path.join(__dirname, 'view'));
+    app.use(express.bodyParser());
+    app.use(express.cookieParser(cookieSessionConfig.cookie.secret));
+    app.use(require('./middleware/dealSession')());
+    app.use(express.cookieSession(cookieSessionConfig.cookieSession));
+
+    app.use(express.methodOverride());
+    app.use(app.routes);
+});
+
+app.configure('development', function(){
+    app.set('port', process.env.PORT || 3000);
+
+    app.use(express.logger('dev'));
+    app.use(express.errorHandler());
+});
+
+app.configure('production', function(){
+    app.set('port', process.env.PORT || 80);
+});
+
+// add static domain in params while render
 var render = require('jade').renderFile;
 app.engine('jade', function(path, options, fn){
-    // add static domain
-    options.DOMAINS = domains;
+    options.DOMAINS = domains[process.env.NODE_ENV || 'development'];
     return render(path, options, fn);
 });
-app.set('view engine', 'jade');
-
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-
-app.use(express.cookieParser('JustASimpleSecretForCookie'));
-app.use(dealSession());
-app.use(express.cookieSession({
-    key: 'WhoAmI',
-    secret: 'JustASimpleSecretForSession',
-    cookie: {
-        maxAge: 1000 * 60 * 60 * 24 * 7,
-        httpOnly: false
-    }
-}));
-
-app.use(express.methodOverride());
-app.use(app.routes);
-app.use(express.static(path.join(__dirname, '../static')));
-
-// development only
-if ('development' == app.get('env')) {
-    app.use(express.errorHandler());
-}
 
 var route;
 for (var i = 0; i < routes.length; i++) {
